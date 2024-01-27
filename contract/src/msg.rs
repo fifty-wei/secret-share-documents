ae952913dd60cb09065ff35210c1e1d4b04f6be5
rere <regis-graptin@sfr.fr>
2024-01-27 17:53:46 +0100

    feat: generate public/private key at contract initialization allowing future encoded message to the contract

--- contract/src/msg.rs
+++ contract/src/msg.rs
@@ -1,43 +1,39 @@
 use cosmwasm_std::Addr;
+use cosmwasm_std::Binary;
 
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
-pub struct InstantiateMsg {
-      pub count: i32,
-}
+pub struct InstantiateMsg {}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
     StoreNewFile {
         owner: Addr,
-        payload: String
+        payload: String,
     },
-    Increment {},
-    Reset {
-      count: i32
+    EncryptedFilePayload {
+        payload: Binary,
+        public_key: Vec<u8>,
     },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
-    GetFileContent {
-        key: String,
-    },
+    GetFileContent { key: String },
+    GetContractKey {},
 }
 
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+pub struct FilePayloadResponse {
+    pub payload: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
-pub struct FilePayloadResponse {
-    pub payload: String,
+pub struct ContractKeyResponse {
+    pub public_key: Vec<u8>,
 }
