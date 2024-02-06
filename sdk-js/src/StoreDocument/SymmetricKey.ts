8d34d34a7a7539ed2747184f42b7f06cb2550442
Florian Truchot <florian@truchot.co>
2024-02-06 12:43:47 +0100

    fix: remove unused comment

--- sdk-js/src/StoreDocument/SymmetricKey.ts
+++ sdk-js/src/StoreDocument/SymmetricKey.ts
@@ -8,7 +8,6 @@ interface EncryptedData {
 
 const algorithm = "aes-256-gcm";
 
-// Generate symmetric key (256 bits for AES)
 function generate() {
   const key = crypto.randomBytes(32);
   console.log("Symmetric key (hex):", key.toString("hex"));
@@ -16,28 +15,28 @@ function generate() {
 }
 
 function encrypt(text: string, publicKey: Buffer): EncryptedData {
-  const initialVector = crypto.randomBytes(16); // Move IV generation inside encrypt to ensure uniqueness per encryption
+  const initialVector = crypto.randomBytes(16);
   let cipher = crypto.createCipheriv(algorithm, publicKey, initialVector);
   let encrypted = cipher.update(text);
   encrypted = Buffer.concat([encrypted, cipher.final()]);
-  const authTag = cipher.getAuthTag(); // Get authentication tag after finishing encryption
+  const authTag = cipher.getAuthTag();
   return {
     initialVector: initialVector.toString("hex"),
     data: encrypted.toString("hex"),
-    authTag: authTag.toString("hex"), // Return the authTag as part of the encrypted data
+    authTag: authTag.toString("hex"),
   };
 }
 
 function decrypt(encryptedData: EncryptedData, publicKey: Buffer): string {
   const initialVectorBuffer = Buffer.from(encryptedData.initialVector, "hex");
   const encryptedText = Buffer.from(encryptedData.data, "hex");
-  const authTag = Buffer.from(encryptedData.authTag, "hex"); // Extract the authTag
+  const authTag = Buffer.from(encryptedData.authTag, "hex");
   let decipher = crypto.createDecipheriv(
     algorithm,
     publicKey,
     initialVectorBuffer,
   );
-  decipher.setAuthTag(authTag); // Set the authentication tag before decryption
+  decipher.setAuthTag(authTag);
   let decrypted = decipher.update(encryptedText);
   decrypted = Buffer.concat([decrypted, decipher.final()]);
   return decrypted.toString();
