c3bdd055dd30612979f5417f981de22d83c79fef
Florian Truchot <florian@truchot.co>
2024-02-06 12:40:42 +0100

    fix: symmetric key algorithm with tests

--- sdk-js/tests/symetric-key.test.ts
+++ sdk-js/tests/symetric-key.test.ts
@@ -2,17 +2,14 @@ import SymmetricKey from "../src/StoreDocument/SymmetricKey";
 import { test, expect } from "@jest/globals";
 
 test("Generate symmetric key is not empty", async () => {
-  const key = await SymmetricKey.generate();
+  const key = SymmetricKey.generate();
   expect(key).toBeDefined();
 });
 
 test("Text is the same after decryption", async () => {
-  const key = await SymmetricKey.generate();
   const text = "fakeText";
-  const encryptedData = SymmetricKey.encrypt(text, key.toString("hex"));
-  const decryptedText = SymmetricKey.decrypt(
-    encryptedData,
-    key.toString("hex"),
-  );
+  const publicKey = SymmetricKey.generate();
+  const encryptedData = SymmetricKey.encrypt(text, publicKey);
+  const decryptedText = SymmetricKey.decrypt(encryptedData, publicKey);
   expect(decryptedText).toBe(text);
 });
