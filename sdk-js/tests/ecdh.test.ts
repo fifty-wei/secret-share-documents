d6e7e506fc9afc1f8f2e8f878bcd784422bc7b8f
Florian Truchot <florian@truchot.co>
2024-02-10 14:49:49 +0100

    feat: add encrypt data with shared keys

--- sdk-js/tests/ecdh.test.ts
+++ sdk-js/tests/ecdh.test.ts
@@ -4,12 +4,46 @@ import ECDHEncryption from "../src/StoreDocument/Encryption/ECDHEncryption";
 import SecretNetworkIntergration from "../src/SmartContract/SecretNetworkIntegration";
 import { Wallet } from "secretjs";
 
-
 test("Generate ECDH Key Pairs", async () => {
   const ECDHKeys = ECDHEncryption.generate();
 
-  console.log('[INFO] ECDH Keys:', { ECDHKeys });
+  console.log("[INFO] ECDH Keys:", { ECDHKeys });
   expect(ECDHKeys).toBeDefined();
-  expect(ECDHKeys).toHaveProperty('privateKey');
-  expect(ECDHKeys).toHaveProperty('publicKey');
+  expect(ECDHKeys).toHaveProperty("privateKey");
+  expect(ECDHKeys).toHaveProperty("publicKey");
+});
+
+test("Generate ECDH shared keys", async () => {
+  const ECDHKeysA = ECDHEncryption.generate();
+  const ECDHKeysB = ECDHEncryption.generate();
+
+  const sharedKeyA = ECDHEncryption.generateSharedKey(
+    ECDHKeysB.publicKey,
+    ECDHKeysA.privateKey,
+  );
+  const sharedKeyB = ECDHEncryption.generateSharedKey(
+    ECDHKeysA.publicKey,
+    ECDHKeysB.privateKey,
+  );
+
+  console.log("[INFO] ECDH shared Keys:", { sharedKeyA }, { sharedKeyB });
+  expect(sharedKeyA).toBeDefined();
+  expect(sharedKeyB).toBeDefined();
+  expect(sharedKeyA).toEqual(sharedKeyA);
+});
+
+test("Encrypt Data with ECDH shared key", async () => {
+  const ECDHKeysA = ECDHEncryption.generate();
+  const ECDHKeysB = ECDHEncryption.generate();
+
+  const sharedKey = ECDHEncryption.generateSharedKey(
+    ECDHKeysB.publicKey,
+    ECDHKeysA.privateKey,
+  );
+
+  const data = { fake: "test" };
+  const encryptedData = await ECDHEncryption.encrypt(data, sharedKey);
+
+  console.log("[INFO] ECDH encryptedData:", { encryptedData });
+  expect(encryptedData).toBeDefined();
 });
