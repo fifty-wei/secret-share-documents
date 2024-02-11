077bb2448e1996cc77d6e5936ea0b7ed9709babe
Florian Truchot <florian@truchot.co>
2024-02-11 17:14:44 +0100

    refactor: sdk client

---
+++ sdk-js/tests/share-document-client.test.ts
@@ -0,0 +1,33 @@
+import { test, expect } from "@jest/globals";
+import { getConfig } from "../config";
+import ShareDocumentClient from "../src";
+import FakeStorage from "../src/StoreDocument/Storage/FakeStorage";
+
+test("Get SDK with our configuration", async () => {
+  const config = await getConfig();
+  const client = new ShareDocumentClient({
+    storage: new FakeStorage(),
+    config: {
+      ...config,
+      evmWalletConfig: {
+        mnemonic: process.env.POLYGON_WALLET_MNEMONIC,
+      },
+    },
+  });
+
+  expect(client).toBeDefined();
+  expect(client.storeDocument()).toBeInstanceOf(ShareDocumentClient);
+
+  const responseUrl = await client
+    .storeDocument()
+    .fromUrl("https://school.truchot.co/ressources/brief-arolles-bis.pdf");
+
+  const responseFile = await client
+    .storeDocument()
+    .fromFile(
+      new File([""], "brief-arolles-bis.pdf", { type: "application/pdf" }),
+    );
+
+  expect(responseUrl).toBeDefined();
+  expect(responseFile).toBeDefined();
+});
