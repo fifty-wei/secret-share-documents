c805f31cfee4f66e03b4b48c0917e0155969ab32
Florian Truchot <florian@truchot.co>
2024-02-19 12:25:08 +0100

    fix: Share Document tests

--- sdk-js/tests/share-document-smart-contract.test.ts
+++ sdk-js/tests/share-document-smart-contract.test.ts
@@ -1,54 +1,48 @@
 import { expect, test } from "@jest/globals";
 import ShareDocumentSmartContract from "../src/SmartContract/ShareDocumentSmartContract";
-import SecretNetworkIntegration from "../src/SmartContract/SecretNetworkIntegration";
-import { Wallet } from "secretjs";
-import { getConfig } from "../config";
+import { SecretNetworkClient, Wallet } from "secretjs";
+import Config from "../src/Config";
+import dotenv from "dotenv";
+import Environment from "../src/Environment";
 
-test("Get public key", async () => {
-  const config = await getConfig();
+dotenv.config();
 
-  const wallet = new Wallet(process.env.SECRET_NETWORK_WALLET_MNEMONIC);
-  const secretNetwork = new SecretNetworkIntegration({
-    endpoint: config.chains.secretNetwork.endpoint,
-    chainId: config.chains.secretNetwork.chainId,
-    faucetEndpoint: config.chains.secretNetwork.faucetEndpoint,
-    wallet: wallet,
-  });
+const config = new Config({
+  env: Environment.TESTNET,
+});
+const wallet = new Wallet(process.env.SECRET_NETWORK_WALLET_MNEMONIC);
 
+const secretNetworkClient = new SecretNetworkClient({
+  url: config.getSecretNetwork().endpoint,
+  chainId: config.getSecretNetwork().chainId,
+  wallet: wallet,
+  walletAddress: wallet.address,
+});
+
+test("Get ShareDocument public key", async () => {
   const shareDocument = new ShareDocumentSmartContract({
-    chainId: config.chains.secretNetwork.chainId,
-    client: secretNetwork.getClient(),
-    contract: config.contracts.ShareDocument,
+    chainId: config.getSecretNetwork().chainId,
+    client: secretNetworkClient,
+    contract: config.getShareDocument(),
     wallet: wallet,
   });
 
   const publicKey = await shareDocument.getPublicKey();
-  console.log("[INFO] Get publicKey:", { publicKey });
 
   expect(publicKey).toBeDefined();
 }, 1_000_000);
 
-test("Get permit", async () => {
-  const config = await getConfig();
-
-  const wallet = new Wallet(process.env.SECRET_NETWORK_WALLET_MNEMONIC);
-  const secretNetwork = new SecretNetworkIntegration({
-    endpoint: config.chains.secretNetwork.endpoint,
-    chainId: config.chains.secretNetwork.chainId,
-    faucetEndpoint: config.chains.secretNetwork.faucetEndpoint,
-    wallet: wallet,
-  });
-
+test("Get ShareDocument permit", async () => {
   const shareDocument = new ShareDocumentSmartContract({
-    chainId: config.chains.secretNetwork.chainId,
-    client: secretNetwork.getClient(),
-    contract: config.contracts.ShareDocument,
+    chainId: config.getSecretNetwork().chainId,
+    client: secretNetworkClient,
+    contract: config.getShareDocument(),
     wallet: wallet,
   });
 
   const permit = await shareDocument.generatePermit();
 
-  console.log("[INFO] Get permit:", { permit });
-
   expect(permit).toBeDefined();
+  expect(permit).toHaveProperty("signature");
+  expect(permit).toHaveProperty("params");
 }, 1_000_000);
