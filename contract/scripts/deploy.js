2f51cb1febe5f50c0a6dec9ad4633700f556605b
rere <regis-graptin@sfr.fr>
2024-02-22 21:21:05 +0100

    feat: deploy smart contract on testnet

---
+++ contract/scripts/deploy.js
@@ -0,0 +1,79 @@
+import { SecretNetworkClient, Wallet } from "secretjs";
+import * as fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+
+const wallet = new Wallet(process.env.MNEMONIC);
+const contract_wasm = fs.readFileSync("../contract.wasm.gz");
+
+const secretjs = new SecretNetworkClient({
+  chainId: "pulsar-3",
+  url: "https://api.pulsar.scrttestnet.com",
+  wallet: wallet,
+  walletAddress: wallet.address,
+});
+
+console.log(secretjs)
+
+
+
+let upload_contract = async () => {
+  let tx = await secretjs.tx.compute.storeCode(
+    {
+      sender: wallet.address,
+      wasm_byte_code: contract_wasm,
+      source: "",
+      builder: "",
+    },
+    {
+      gasLimit: 4_000_000,
+    }
+  );
+
+  let codeId = Number(
+    tx.arrayLog.find((log) => log.type === "message" && log.key === "code_id")
+      .value
+  );
+
+  console.log("codeId: ", codeId);
+
+  let contractCodeHash = (
+    await secretjs.query.compute.codeHashByCodeId({ code_id: codeId })
+  ).code_hash;
+  console.log(`Contract hash: ${contractCodeHash}`);
+  
+  return codeId, contractCodeHash;
+};
+
+// let codeId, contractCodeHash = await upload_contract();
+
+let codeId = "4890";
+let contractCodeHash = "28aa8b90638e8f47240695b4f0c4a027f7e2991373c618da6d3d8b1daf7dbc0a";
+
+let instantiate_contract = async (codeId, contractCodeHash) => {
+
+  console.log(codeId, contractCodeHash)
+
+  const initMsg = {};
+  let tx = await secretjs.tx.compute.instantiateContract(
+    {
+      code_id: codeId,
+      sender: wallet.address,
+      code_hash: contractCodeHash,
+      init_msg: initMsg,
+      label: "Secret Contract" + Math.ceil(Math.random() * 10000),
+    },
+    {
+      gasLimit: 400_000,
+    }
+  );
+
+  //Find the contract_address in the logs
+  const contractAddress = tx.arrayLog.find(
+    (log) => log.type === "message" && log.key === "contract_address"
+  ).value;
+
+  console.log(contractAddress);
+};
+
+await instantiate_contract(codeId, contractCodeHash);
---
+++ sdk-js/contract/README.md
@@ -0,0 +1,14 @@
+
+More detail info in `/contract/scripts/README.md`
+
+
+Network info:
+- chainId: "pulsar-3",
+- url: "https://api.pulsar.scrttestnet.com",
+
+
+
+Contract address:
+> secret14tplljk8wjezkya2jcx2ynjx5udue8uj69f75q
+
+> Transaction: https://testnet.ping.pub/secret/tx/B0FF5B3C89EE1911557608FF5950AEA46511E2DDAA36EB547FF94268BFA47009
