c6f03bc3f62bed72e265913d2d12ba8c4b0e4415
Florian Truchot <florian@truchot.co>
2024-02-26 11:38:31 +0100

    fix: view tests

--- sdk-js/tests/view-document.test.ts
+++ sdk-js/tests/view-document.test.ts
@@ -3,48 +3,115 @@ import SecretDocumentSmartContract from "../src/SmartContract/SecretDocumentSmar
 import { SecretNetworkClient, Wallet } from "secretjs";
 import Config from "../src/Config";
 import dotenv from "dotenv";
-import Environment from "../src/Environment";
 import ViewDocument from "../src/ViewDocument";
+import initLocalSecretNetworkSmartContract from "./init-local-secret-document";
+import Environment from "../src/Environment";
+import ViemClient from "../src/SmartContract/ViemClient";
+import StoreDocument from "../src/StoreDocument";
+import PolygonToSecretSmartContract from "../src/SmartContract/PolygonToSecretSmartContract";
+import FakeStorage from "../src/StoreDocument/Storage/FakeStorage";
 
 dotenv.config();
 
-const config = new Config({
-  env: Environment.TESTNET,
-});
-
+const config = new Config();
 const wallet = new Wallet(process.env.SECRET_NETWORK_WALLET_MNEMONIC);
 
-const secretNetworkClient = new SecretNetworkClient({
-  url: config.getSecretNetwork().endpoint,
-  chainId: config.getSecretNetwork().chainId,
-  wallet: wallet,
-  walletAddress: wallet.address,
-});
+async function init() {
+  if (config.getEnv() === Environment.LOCAL) {
+    const contract = await initLocalSecretNetworkSmartContract(config);
+    config.useShareDocument(contract);
+  }
 
-const secretDocument = new SecretDocumentSmartContract({
-  chainId: config.getSecretNetwork().chainId,
-  client: secretNetworkClient,
-  contract: config.getShareDocument(),
-  wallet: wallet,
-});
+  const secretNetworkClient = new SecretNetworkClient({
+    url: config.getSecretNetwork().endpoint,
+    chainId: config.getSecretNetwork().chainId,
+    wallet: wallet,
+    walletAddress: wallet.address,
+  });
 
-const viewDocument = new ViewDocument({
-  secretDocument: secretDocument,
-});
+  const secretDocument = new SecretDocumentSmartContract({
+    chainId: config.getSecretNetwork().chainId,
+    client: secretNetworkClient,
+    contract: config.getShareDocument(),
+    wallet: wallet,
+  });
+
+  const viewDocument = new ViewDocument({
+    secretDocument: secretDocument,
+  });
+
+  const viemClient = new ViemClient({
+    chain: config.getChain(config.getChainId()),
+    walletConfig: config.getEvmWallet(),
+    contract: config.getPolygonToSecret(),
+  });
+
+  const polygonToSecret = new PolygonToSecretSmartContract({
+    secretContract: config.getShareDocument(),
+    viemClient: viemClient,
+  });
+
+  const storeDocument = new StoreDocument({
+    storage: new FakeStorage(),
+    secretDocument: secretDocument,
+    polygonToSecret: polygonToSecret,
+  });
+
+  return {
+    storeDocument,
+    secretDocument,
+    viewDocument,
+  };
+}
+
+async function store({ secretDocument, storeDocument, fileUrl }) {
+  let uploadOptions = {
+    contentType: "application/pdf",
+  };
+  const { data, contentType } = await storeDocument.fetchDocument(fileUrl);
+  uploadOptions.contentType = contentType;
+  const bufferData = Buffer.from(data);
+
+  const encryptedMessage = await storeDocument.getEncryptedMessage(
+    bufferData,
+    uploadOptions,
+  );
+
+  const payload = {
+    source_chain: "test-chain",
+    source_address: "test-address",
+    payload: encryptedMessage,
+  };
+
+  return await secretDocument.store(payload);
+}
 
 test("Get all files the user is allowed acces to", async () => {
+  const { viewDocument, secretDocument, storeDocument } = await init();
+
+  const document = await store({
+    secretDocument: secretDocument,
+    storeDocument: storeDocument,
+    fileUrl: "https://school.truchot.co/ressources/brief-arolles-bis.pdf",
+  });
   const data = await viewDocument.all();
 
   expect(data).toBeDefined();
-  // expect(data).toHaveProperty("payload");
-  // expect(data).toHaveProperty("public_key");
-});
+  expect(data).toHaveLength(1);
+}, 100_000);
 
 test("Find file content from fileId", async () => {
-  const data = await viewDocument.get(
-    "24b4bd2bd6495f74dc1fbd7473292f8fd658d6fede78e6343e2aceb0fdc2b967",
-  );
+  const { viewDocument, secretDocument, storeDocument } = await init();
+
+  const document = await store({
+    secretDocument: secretDocument,
+    storeDocument: storeDocument,
+    fileUrl: "https://school.truchot.co/ressources/brief-arolles-bis.pdf",
+  });
+
+  const allData = await viewDocument.all();
+  const data = await viewDocument.get(allData[0]);
 
   expect(data).toBeDefined();
   expect(data).toHaveProperty("url");
-});
+}, 100_000);
