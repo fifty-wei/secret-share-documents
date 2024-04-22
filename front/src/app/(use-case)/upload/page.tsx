3ed453a8d668a6963d7d03992f67961b73697718
Florian Truchot <florian@truchot.co>
2024-04-22 23:58:53 +0200

    refactor: split header

--- front/src/app/(use-case)/upload/page.tsx
+++ front/src/app/(use-case)/upload/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 
+import Image from "next/image";
 import {useAccount} from "wagmi";
+import {Header} from "@/components/header";
 import {UploadForm} from "@/components/upload-form";
 import {FetchAllFileIds} from "@/components/fetch-all-file-ids";
 import {FileIdsList} from "@/components/file-ids-list";
 import {ConnectWallet} from "@/components/connect-wallet";
 import {Badge} from "@/components/ui/badge";
 import {Button} from "@/components/ui/button";
-import Image from "next/image";
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -36,67 +37,9 @@ export default function UploadPage() {
             <FetchAllFileIds>
                 {({fetchAllFileIds, fileIds, loading, error}) => {
 
-                    const connectClasses = cn('flex items-center gap-2', {
-                        'opacity-30 line-through': isConnected,
-                    });
-
-                    const storeClasses = cn('flex items-center gap-2', {
-                        'opacity-30 line-through': fileIds.length > 0,
-                    });
-
                     return(
                         <>
-                            <header className="flex justify-between items-center px-4 py-4 sm px-6 md justify-start md space-x-10 lg px-8 border-b">
-                                <span className="flex gap-2 items-center">
-                                    <Image
-                                        className=""
-                                        src="/logo.png"
-                                        width={56}
-                                        height={56}
-                                        alt=""
-                                    />
-                                    <span className="text-xl font-semibold">Secret Document</span>
-                                </span>
-
-                                <Breadcrumb className="absolute left-1/2 -translate-x-1/2">
-                                    <BreadcrumbList>
-                                        <BreadcrumbItem>
-                                            <BreadcrumbPage className={connectClasses}>
-                                                <Badge variant="secondary" className="w-6 h-6 inline-flex justify-center items-center">1</Badge>
-                                                Connect with Metamask
-                                            </BreadcrumbPage>
-                                        </BreadcrumbItem>
-                                        <BreadcrumbSeparator />
-                                        <BreadcrumbItem>
-                                            <BreadcrumbPage className={storeClasses}>
-                                                <Badge variant="secondary" className="w-6 h-6 inline-flex justify-center items-center">2</Badge>
-                                                Store your document
-                                            </BreadcrumbPage>
-                                        </BreadcrumbItem>
-                                        <BreadcrumbSeparator />
-                                        <BreadcrumbItem>
-                                            <BreadcrumbPage className={storeClasses}>
-                                                <Badge variant="secondary" className="w-6 h-6 inline-flex justify-center items-center">3</Badge>
-                                                Get all your private documents
-                                            </BreadcrumbPage>
-                                        </BreadcrumbItem>
-                                        <BreadcrumbSeparator />
-                                        <BreadcrumbItem>
-                                            <BreadcrumbPage className="flex items-center gap-2">
-                                                <Badge variant="secondary" className="w-6 h-6 inline-flex justify-center items-center">4</Badge>
-                                                Share it
-                                            </BreadcrumbPage>
-                                        </BreadcrumbItem>
-                                    </BreadcrumbList>
-                                </Breadcrumb>
-
-                                <div className="flex items-center gap-4">
-                                    <Account />
-                                    <DisconnectWallet>
-                                        Disconnect
-                                    </DisconnectWallet>
-                                </div>
-                            </header>
+                            <Header fileIds={fileIds} />
                             <main className="flex-1 grid grid-cols-2 divide-x">
                                 <UploadForm />
                                 <div>
@@ -104,12 +47,12 @@ export default function UploadPage() {
                                     {error && (
                                         <div className="h-full w-full flex flex-col gap-4 items-center justify-center">
                                             <Badge variant="destructive">Error</Badge>
-                                            {error.message}
+                                            <p className="font-mono">{error.message}</p>
                                             <Button onClick={fetchAllFileIds}>Try again</Button>
                                         </div>
                                     )}
                                     {fileIds.length > 0 ? (
-                                        <div className="p-8">
+                                        <div className="p-8 flex flex-col gap-4">
                                             <FileIdsList fileIds={fileIds}/>
                                         </div>
                                     ) : (
---
+++ front/src/components/header.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import {useAccount, useDisconnect} from "wagmi";
+import {Button} from "@/components/ui/button";
+import Image from "next/image";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
+import {Badge} from "@/components/ui/badge";
+import {Account} from "@/components/account";
+import {DisconnectWallet} from "@/components/disconnect-wallet";
+import {cn} from "@/lib/utils";
+
+interface Props {
+    children: (props: { fileIds: string[]; loading: boolean; error: Error | null }) => JSX.Element;
+    className?: string;
+}
+
+export function Header({children, fileIds}: Props) {
+    const { isConnected } = useAccount();
+
+    const connectClasses = cn('flex items-center gap-2', {
+        'opacity-30 line-through': isConnected,
+    });
+
+    const storeClasses = cn('flex items-center gap-2', {
+        'opacity-30 line-through': fileIds.length > 0,
+    });
+
+    return (
+        <header className="flex justify-between items-center px-4 py-4 sm px-6 md justify-start md space-x-10 lg px-8 border-b">
+            <span className="flex gap-2 items-center">
+                <Image
+                    className=""
+                    src="/logo.png"
+                    width={56}
+                    height={56}
+                    alt=""
+                />
+                <span className="text-xl font-semibold">Secret Document</span>
+            </span>
+
+            <Breadcrumb className="absolute left-1/2 -translate-x-1/2">
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbPage className={connectClasses}>
+                            <Badge variant="secondary" className="w-6 h-6 inline-flex justify-center items-center">1</Badge>
+                            Connect with Metamask
+                        </BreadcrumbPage>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage className={storeClasses}>
+                            <Badge variant="secondary" className="w-6 h-6 inline-flex justify-center items-center">2</Badge>
+                            Store your document
+                        </BreadcrumbPage>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage className={storeClasses}>
+                            <Badge variant="secondary" className="w-6 h-6 inline-flex justify-center items-center">3</Badge>
+                            Get all your private documents
+                        </BreadcrumbPage>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage className="flex items-center gap-2">
+                            <Badge variant="secondary" className="w-6 h-6 inline-flex justify-center items-center">4</Badge>
+                            Share it
+                        </BreadcrumbPage>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+
+            <div className="flex items-center gap-4">
+                <Account />
+                <DisconnectWallet>
+                    Disconnect
+                </DisconnectWallet>
+            </div>
+        </header>
+    )
+}
