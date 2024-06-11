ad3da7af0aa67ce016fb625c3b9cc23d0440d4f7
Florian Truchot <florian@truchot.co>
2024-06-11 08:34:55 +0200

    fix: add download method in FakeStorage implementation

--- sdk-js/src/StoreDocument/Storage/FakeStorage.ts
+++ sdk-js/src/StoreDocument/Storage/FakeStorage.ts
@@ -3,12 +3,20 @@ import ISymmetricEncryptedData from "../../Encryption/ISymmetricEncryptedData";
 import IUploadOptions from "./IUploadOptions";
 
 class FakeStorage implements IStorage {
-  async upload(
-    encryptedData: ISymmetricEncryptedData,
-    options: IUploadOptions,
-  ): Promise<any> {
-    return `https://fake.net/1234567890`;
-  }
+    async upload(
+        encryptedData: ISymmetricEncryptedData,
+        options: IUploadOptions,
+    ): Promise<any> {
+        return `https://fake.net/1234567890`;
+    }
+
+    async download(url: string): Promise<ISymmetricEncryptedData> {
+        return {
+            initialVector: '',
+            data: Buffer.from('fake download'),
+            authTag: ''
+        };
+    }
 }
 
 export default FakeStorage;
